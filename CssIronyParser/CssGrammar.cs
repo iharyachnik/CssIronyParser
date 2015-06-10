86a4470d1eaca63cd759dba9de7e5fb3684e2382
Ihar Yachnik <jutigor@yandex.ru>
2015-06-10 15:14:11 +0300

    Add Irony.dll

--- CssIronyParser/CssGrammar.cs
+++ CssIronyParser/CssGrammar.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Irony.Parsing;
 
 namespace CssIronyParser
 {
     public class CssGrammar
     {
+
+        // Grammar version 2.1 http://www.w3.org/TR/CSS21/grammar.html
     }
 }
